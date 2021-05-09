397fefefde56770e4ebe38530b213af92939882b
Guilherme Miguel <guilherme.souza1996@hotmail.com>
2021-05-09 09:46:30 -0300

    Estruturando o projeto

---
+++ Carga.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace CargaCarro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o diretório do arquivo");
+
+            var diretorio = Console.ReadLine();
+
+            var arquivo = Directory.GetFiles(diretorio);
+
+            if(Path.GetExtension(arquivo) )
+        }
+    }
+}
